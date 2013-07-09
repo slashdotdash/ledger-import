ca772703069441c8214d102389f2ce236289ae87
Ben Smith <ben@slashdotdash.net>
2013-07-09 17:48:50 +0100

    Updated expense specifications for all supported spreadsheet formats.

--- lib/specifications/expenses.rb
+++ lib/specifications/expenses.rb
@@ -19,60 +19,14 @@ module Specifications
           'F' => 'Liabilities:Credit Card'
         }
       }
-        
-      motor = Specification.new(defaults.merge({ :to => 'Expenses:Motor', :amount => 'H' }))
-      travel = Specification.new(defaults.merge({ :to => 'Expenses:Travel', :amount => 'I' }))
-      insurance = Specification.new(defaults.merge({ :to => 'Expenses:Insurance', :amount => 'J' }))
-      pension = Specification.new(defaults.merge({ :to => 'Expenses:Pension', :amount => 'K' }))
-      salary = Specification.new(defaults.merge({ :to => 'Expenses:Salary', :amount => 'L' }))
-      paye_ni = Specification.new(defaults.merge({ :to => 'Expenses:Tax:PAYE/NI', :amount => 'M' }))
-      dividend = Specification.new(defaults.merge({ :to => 'Expenses:Dividend', :amount => 'N' }))
-      cost_of_goods_sold = Specification.new(defaults.merge({ :to => 'Expenses:Goods Sold', :amount => 'O' }))
-      stationery = Specification.new(defaults.merge({ :to => 'Expenses:Stationery', :amount => 'P' }))
-      internet = Specification.new(defaults.merge({ :to => 'Expenses:Internet', :amount => 'Q' }))
-      assets_purchased = Specification.new(defaults.merge({ :to => 'Expenses:Assets Purchased', :amount => 'R' }))
-      computer = Specification.new(defaults.merge({ :to => 'Expenses:Computer', :amount => 'S' }))
-      subscriptions = Specification.new(defaults.merge({ :to => 'Expenses:Subscriptions', :amount => 'T' }))
-      accountancy = Specification.new(defaults.merge({ :to => 'Expenses:Accountancy', :amount => 'U' }))
-      bank_charges = Specification.new(defaults.merge({ :to => 'Expenses:Bank Charges', :amount => 'V' }))
-      home_use = Specification.new(defaults.merge({ :to => 'Expenses:Home', :amount => 'W' }))
-      deposit_account = Specification.new(defaults.merge({ :to => 'Assets:Deposit Account', :amount => 'X' }))
-      vat = Specification.new(defaults.merge({ :to => 'Expenses:Tax:VAT', :amount => 'Y' }))
-      corporation_tax = Specification.new(defaults.merge({ :to => 'Expenses:Tax:Corporation', :amount => 'Z' }))
-      directors_loan = Specification.new(defaults.merge({ :to => 'Expenses:Directors Loan', :amount => 'AA' }))
-      entertainment = Specification.new(defaults.merge({ :to => 'Expenses:Entertainment', :amount => 'AB' }))
-      sundries = Specification.new(defaults.merge({ :to => 'Expenses:Sundries', :amount => 'AC' }))
+      
+      expenses = expenses_for_format(defaults)
       
       # reclaimed expenses
-      reimbursed_cash = Specification.new(defaults.merge({ :to => 'Expenses:Cash', :from => 'Assets:Current Account', :amount => ['D', 'E'] }))
-      reimbursed_credit_card = Specification.new(defaults.merge({ :to => 'Liabilities:Credit Card', :from => 'Assets:Current Account', :amount => ['D', 'F'] }))
+      expenses << Specification.new(defaults.merge({ :to => 'Expenses:Cash', :from => 'Assets:Current Account', :amount => ['D', 'E'] }))
+      expenses << Specification.new(defaults.merge({ :to => 'Liabilities:Credit Card', :from => 'Assets:Current Account', :amount => ['D', 'F'] }))
       
-      [ 
-        motor, 
-        travel, 
-        insurance, 
-        pension, 
-        salary, 
-        paye_ni, 
-        dividend, 
-        cost_of_goods_sold, 
-        stationery, 
-        internet, 
-        assets_purchased,
-        computer,
-        subscriptions,
-        accountancy,
-        bank_charges,
-        home_use,
-        deposit_account,
-        vat,
-        corporation_tax,
-        directors_loan,
-        entertainment,
-        sundries,
-        reimbursed_cash,
-        reimbursed_credit_card
-      ]
+      expenses
     end
   
   private
@@ -82,6 +36,65 @@ module Specifications
           (80..196)
         when '2011-12', '2010-11', '2009-10', '2008-09'
           (69..99)
+        else 
+          throw 'Format not recognised'
+      end
+    end
+    
+    def expenses_for_format(defaults)
+      case @format
+        when '2013-14', '2012-13', '2011-12', '2010-11'
+          [ 
+            Specification.new(defaults.merge({ :to => 'Expenses:Motor', :amount => 'H' })),
+            Specification.new(defaults.merge({ :to => 'Expenses:Travel', :amount => 'I' })),
+            Specification.new(defaults.merge({ :to => 'Expenses:Insurance', :amount => 'J' })),
+            Specification.new(defaults.merge({ :to => 'Expenses:Pension', :amount => 'K' })),
+            Specification.new(defaults.merge({ :to => 'Expenses:Salary', :amount => 'L' })),
+            Specification.new(defaults.merge({ :to => 'Expenses:Tax:PAYE/NI', :amount => 'M' })),
+            Specification.new(defaults.merge({ :to => 'Expenses:Dividend', :amount => 'N' })),
+            Specification.new(defaults.merge({ :to => 'Expenses:Goods Sold', :amount => 'O' })),
+            Specification.new(defaults.merge({ :to => 'Expenses:Stationery', :amount => 'P' })),
+            Specification.new(defaults.merge({ :to => 'Expenses:Internet', :amount => 'Q' })),
+            Specification.new(defaults.merge({ :to => 'Expenses:Assets Purchased', :amount => 'R' })),
+            Specification.new(defaults.merge({ :to => 'Expenses:Computer', :amount => 'S' })),
+            Specification.new(defaults.merge({ :to => 'Expenses:Subscriptions', :amount => 'T' })),
+            Specification.new(defaults.merge({ :to => 'Expenses:Accountancy', :amount => 'U' })),
+            Specification.new(defaults.merge({ :to => 'Expenses:Bank Charges', :amount => 'V' })),
+            Specification.new(defaults.merge({ :to => 'Expenses:Home', :amount => 'W' })),
+            Specification.new(defaults.merge({ :to => 'Assets:Deposit Account', :amount => 'X' })),
+            Specification.new(defaults.merge({ :to => 'Expenses:Tax:VAT', :amount => 'Y' })),
+            Specification.new(defaults.merge({ :to => 'Expenses:Tax:Corporation', :amount => 'Z' })),
+            Specification.new(defaults.merge({ :to => 'Expenses:Directors Loan', :amount => 'AA' })),
+            Specification.new(defaults.merge({ :to => 'Expenses:Entertainment', :amount => 'AB' })),
+            Specification.new(defaults.merge({ :to => 'Expenses:Sundries', :amount => 'AC' })),
+          ]
+        when '2009-10', '2008-09'
+          [
+            Specification.new(defaults.merge({ :to => 'Expenses:Motor', :amount => 'H' })),
+            Specification.new(defaults.merge({ :to => 'Expenses:Travel', :amount => 'I' })),
+            Specification.new(defaults.merge({ :to => 'Expenses:Accommodation', :amount => 'J' })),
+            Specification.new(defaults.merge({ :to => 'Expenses:Insurance', :amount => 'K' })),
+            Specification.new(defaults.merge({ :to => 'Expenses:Pension', :amount => 'L' })),
+            Specification.new(defaults.merge({ :to => 'Expenses:Goods Sold', :amount => 'M' })),
+            Specification.new(defaults.merge({ :to => 'Expenses:Salary', :amount => 'N' })),
+            Specification.new(defaults.merge({ :to => 'Expenses:Tax:PAYE/NI', :amount => 'O' })),
+            Specification.new(defaults.merge({ :to => 'Expenses:Salary', :amount => 'P' })),
+            Specification.new(defaults.merge({ :to => 'Expenses:Stationery', :amount => 'Q' })),
+            Specification.new(defaults.merge({ :to => 'Expenses:Internet', :amount => 'R' })),
+            Specification.new(defaults.merge({ :to => 'Expenses:Assets Purchased', :amount => 'S' })),
+            Specification.new(defaults.merge({ :to => 'Expenses:Computer', :amount => 'T' })),
+            Specification.new(defaults.merge({ :to => 'Expenses:Tax:VAT', :amount => 'U' })),
+            Specification.new(defaults.merge({ :to => 'Expenses:Subscriptions', :amount => 'V' })),
+            Specification.new(defaults.merge({ :to => 'Expenses:Dividend', :amount => 'W' })),
+            Specification.new(defaults.merge({ :to => 'Expenses:Accountancy', :amount => 'X' })),
+            Specification.new(defaults.merge({ :to => 'Assets:Deposit Account', :amount => 'Y' })),
+            Specification.new(defaults.merge({ :to => 'Expenses:Bank Charges', :amount => 'Z' })),
+            Specification.new(defaults.merge({ :to => 'Expenses:Home', :amount => 'AA' })),
+            Specification.new(defaults.merge({ :to => 'Expenses:Tax:Corporation', :amount => 'AB' })),
+            Specification.new(defaults.merge({ :to => 'Expenses:Directors Loan', :amount => 'AC' })),
+            Specification.new(defaults.merge({ :to => 'Expenses:Entertainment', :amount => 'AD' })),
+            Specification.new(defaults.merge({ :to => 'Expenses:Sundries', :amount => 'AE' })),
+          ]
         else 
           throw 'Format not recognised'
       end
